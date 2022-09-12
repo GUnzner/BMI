a930bdac682b62b8a4e1a20c84bbecaa18cb8a99
GUnzner <grit.unzner@googlemail.com>
2022-09-12 18:11:25 +0200

    bug fix

--- script.js
+++ script.js
@@ -1,17 +1,27 @@
 function getBMI() {
 
-    var height = document.getElementById("height").value;
-    var weight = document.getElementById("weight").value;
-    var BMI = weight/Math.pow(height/100, 2);
+    let height = document.getElementById("height").value;
+    let weight = document.getElementById("weight").value;
+
+    if(document.getElementById("heightTag").innerText == "Height in feet:"){
+        height = (height * 12 + parseFloat(document.getElementById("height-2").value)) * 2.54
+        weight /= 2.2046
+    } else {
+        height += document.getElementById("height-2").value;
+    }
+    
+    //console.log(height, weight);
+    
+    let BMI = weight/Math.pow(height/100, 2);
     
     console.log(BMI)
     
-    var bmiText = "Your BMI is "+BMI.toFixed(2)+"<br>"
-    var result = document.getElementById("text")
+    let bmiText = "Your BMI is "+BMI.toFixed(2)+"<br>"
+    let result = document.getElementById("text")
     result.innerHTML=bmiText
     
     if(BMI<20){
-        var comment = document.createTextNode("You are underweight.")
+        let comment = document.createTextNode("You are underweight.")
         document.getElementById("text").appendChild(comment);
     } else if(BMI>=20 && BMI<25){
         result.innerHTML += "You are normal weight."
@@ -26,3 +36,15 @@ document.getElementById("weight")
         document.getElementById("btn").click();
     }
 });
+
+function switchUnit(){
+    if (document.getElementById("heightTag").innerText == "Height in m:"){
+        document.getElementById("heightTag").innerText="Height in feet:";
+        document.getElementById("heightTag-2").innerText="and inches:";
+        document.getElementById("weightTag").innerText="Weight in pounds:";
+    } else if (document.getElementById("heightTag").innerText == "Height in feet:"){
+        document.getElementById("heightTag").innerText="Height in m:";
+        document.getElementById("heightTag-2").innerText="and cm:";
+        document.getElementById("weightTag").innerText="Weight in kg:";
+    }
+}
